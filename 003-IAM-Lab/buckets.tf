46897eace18c38f79d72203608b3d3025ed6bce9
sudheer gupta <sudheergupta0508@gmail.com>
2026-08-17 08:31:11 +0000

    updating resources

--- 003-IAM-Lab/buckets.tf
+++ 003-IAM-Lab/buckets.tf
@@ -2,6 +2,6 @@ resource "aws_s3_bucket" "throw-away-bucket-v1" {
     bucket = "throw-away-bucket-v1"
     tags = {
         Name = "Demo"
-        Environment = "Research and Development"
+        Environment = "Product Development"
     } 
 }
