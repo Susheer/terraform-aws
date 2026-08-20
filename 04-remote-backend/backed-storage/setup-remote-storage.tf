aa3c3a01b74f2d5b7c9b6160f5af6c0533017bdb
sudheer gupta <sudheergupta0508@gmail.com>
2026-08-20 18:27:42 +0000

    backend bucket - create this first.

---
+++ 04-remote-backend/backed-storage/setup-remote-storage.tf
@@ -0,0 +1,22 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 6.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "ap-south-1"
+    profile = "terraform"
+}
+
+resource "aws_s3_bucket" "backend-storage" {
+    bucket = "remote-backend-storage-bucket"
+    tags = {
+        team = "DevOps"
+        backend = "backend-storage"
+        version = "v1"
+    }
+}
