62c1547ba5784c9088f6e966c9538631ac1cd270
sudheer gupta <sudheergupta0508@gmail.com>
2026-08-20 18:20:57 +0000

    have the backed storage created frst

--- 04-remote-backend/remote-backend-setup.tf
+++ 04-remote-backend/remote-backend-setup.tf
@@ -1,4 +1,11 @@
 terraform {
+    backend "s3" {
+        bucket = "backend-storage"
+        region = "ap-south-1"
+        key = "dev/terraform.tfstate"
+        encrypt = true
+        use_lockfile = true
+    }
     required_providers {
         aws = {
             source = "hashicorp/aws"
