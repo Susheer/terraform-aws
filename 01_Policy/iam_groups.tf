257f73b019753d8b71510cb13c9808c9b97ef71e
sudheer gupta <sudheergupta0508@gmail.com>
2026-08-21 14:31:54 +0000

    add HR user in the HR Group

--- 01_Policy/iam_groups.tf
+++ 01_Policy/iam_groups.tf
@@ -8,5 +8,11 @@ resource "aws_iam_group" "HR" {
     path = "/users/"
 }
 
-
-
+resource "aws_iam_group_membership" "hr_members" {
+    name = "hr_members"
+    users = [
+        aws_iam_user.Lakhwa.name,
+        aws_iam_user.pushpa.name
+    ]
+    group = aws_iam_group.HR.name
+}
