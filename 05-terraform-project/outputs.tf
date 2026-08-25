8332e59f135436fa31e51940ea956d5b0d0f20ba
spgupta <sgupta@bydefense.com>
2026-08-25 15:15:59 +0530

    Output could be groupped too

--- 05-terraform-project/outputs.tf
+++ 05-terraform-project/outputs.tf
@@ -5,5 +5,6 @@ output "Krishna" {
 
 output "Hr" {
     description = "A group is created to ref HR"
-    value = aws_iam_group.HR.name
+    value = [aws_iam_group.HR.name, aws_iam_user.KishanSharma.name]
 }
+
