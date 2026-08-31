2b6c62e433971825f9a74fa2582cd6f1c99cb08c
sudheer gupta <sudheergupta0508@gmail.com>
2026-08-31 08:42:39 +0000

    No hardcoding on index

--- 06-working-complex-tyeps/main.tf
+++ 06-working-complex-tyeps/main.tf
@@ -29,19 +29,12 @@ resource "aws_iam_group" "HR" {
 # Assign users into the groups
 resource "aws_iam_group_membership" "hr_members" {
     name = "hr_members"
-    users = [
-        aws_iam_user.HRs[0].name,
-        aws_iam_user.HRs[1].name
-    ]
+    users = aws_iam_user.HRs[*].name
     group = aws_iam_group.HR.name
 }
 
 resource "aws_iam_group_membership" "dev_members" {
     name = "dev_members"
     group = aws_iam_group.developers.name
-    users = [
-        aws_iam_user.Developers[0].name,
-        aws_iam_user.Developers[1].name,
-        aws_iam_user.Developers[2].name
-    ]
+    users = aws_iam_user.Developers[*].name
 }
